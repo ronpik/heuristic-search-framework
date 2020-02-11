36a636a8f1090d3ef5f3f1205e9a62ec77135543
ronpik <ronpi1310@gmail.com>
2020-02-12 01:24:14 +0200

    analyzer show some info

--- examples/example.py
+++ examples/example.py
@@ -2,19 +2,27 @@ from operator import itemgetter
 
 from hesearch.algorithms import UniformCostSearch
 from hesearch.experiments.real_distance_pathfinder import RealDistanceGraphProblem, RealDistanceHeuristic
+from hesearch.framework.analysis.problem_analysis import SearchSpaceAnalysisWrapper
 
 if __name__ == "__main__":
+    # initialize search space
     problem = RealDistanceGraphProblem(size=100, seed=71070)
+    problem_analyzer = SearchSpaceAnalysisWrapper(problem)
+
+    # use uniform cost search algorithm
     ucs_solver = UniformCostSearch()
-    ucs_solver.set_search_space(problem)
+    ucs_solver.set_search_space(problem_analyzer)
     ucs_solver.search()
     cost = ucs_solver.get_optimal_cost()
     optimal_path = ucs_solver.get_optimal_path()
 
+    # display some info
     print(cost)
     optimal_path = [node.get_id() for node in optimal_path]
     print(" -> ".join(map(str, optimal_path)))
+    problem_analyzer.show_analysis()
     problem.show_solution(optimal_path)
+
 
 
     # heuristic = RealDistanceHeuristic(problem)
--- hesearch/framework/analysis/problem_analysis.py
+++ hesearch/framework/analysis/problem_analysis.py
@@ -1,3 +1,7 @@
+import json
+from collections import Counter
+from itertools import groupby
+from operator import itemgetter
 from typing import Iterable, Tuple
 
 from hesearch.framework.problem import SearchSpace, SearchState
@@ -5,10 +9,12 @@ from hesearch.framework.problem import SearchSpace, SearchState
 
 class SearchSpaceAnalysisWrapper(SearchSpace):
 
-    def __init__(self, problem: SearchSpace):
-        self.problem = problem
+    def __init__(self, search_space: SearchSpace):
+        self.search_space = search_space
 
         self.data_file = None
+
+        self.nodes_counts = Counter()
 
         self.nodes_generated = 0
         self.goals_found = 0
@@ -19,12 +25,29 @@ class SearchSpaceAnalysisWrapper(SearchSpace):
         self.num_steps = 0
 
     def generate_children(self, state: SearchState) -> Iterable[Tuple[SearchState, float]]:
-        pass
+        children = self.search_space.generate_children(state)
+        for c, cost in children:
+            child_id = c.get_id()
+            self.nodes_counts[child_id] += 1
+            yield c, cost
 
     def is_goal(self, state: SearchState) -> bool:
-        pass
+        is_goal_answer = self.search_space.is_goal(state)
+        if is_goal_answer:
+            self.goals_found += 1
+
+        return is_goal_answer
 
     def get_initial_state(self) -> SearchState:
-        pass
+        return self.search_space.get_initial_state()
+
+    def show_analysis(self):
+        print(f"number of times goal was generated: {self.goals_found}")
+        for num_generated, nodes in groupby(self.nodes_counts.most_common(), key=itemgetter(1)):
+            nodes = list(nodes)
+            print(f"{len(nodes)} nodes have been generated {num_generated} times: {nodes}")
+
+        total_generated = sum(self.nodes_counts.values())
+        print(f"total nodes generated: {total_generated}")
 
 
